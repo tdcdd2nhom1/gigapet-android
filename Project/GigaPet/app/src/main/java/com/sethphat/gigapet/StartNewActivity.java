38bb8d0742f62159ace59d4a07b253bf3a3981bb
Phat TRan <prosandaru@gmail.com>
2018-03-21 10:49:19 +0700

    Finish template: Start New Game, Main Game.

--- Project/GigaPet/app/src/main/java/com/sethphat/gigapet/StartNewActivity.java
+++ Project/GigaPet/app/src/main/java/com/sethphat/gigapet/StartNewActivity.java
@@ -2,6 +2,9 @@ package com.sethphat.gigapet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.sethphat.gigapet.Common.OnSwipeTouchListener;
 
 public class StartNewActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class StartNewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_new_layout);
+
+        // Get main
+        RelativeLayout rlChoosePet = (RelativeLayout) findViewById(R.id.rlChoosePet);
+
+        // action change pet when swipe left - right
+        rlChoosePet.setOnTouchListener(new OnSwipeTouchListener(StartNewActivity.this) {
+            @Override
+            public void onSwipeLeft() {
+                super.onSwipeLeft();
+            }
+
+            @Override
+            public void onSwipeRight() {
+                super.onSwipeRight();
+            }
+        });
     }
 }
